e58a200dd3ba2f88d765d6ef1217f7c61609d290
miyagawa <miyagawa@d0d07461-0603-4401-acd4-de1884942a52>
2007-11-13 01:58:35 +0000

    support exntended pictograms in docomo, and also log the English names and SJIS hex code

--- tools/docomo-uni2name.pl
+++ tools/docomo-uni2name.pl
@@ -6,15 +6,40 @@ use Web::Scraper;
 use URI;
 use YAML;
 
-my $uri = URI->new(
-    "http://www.nttdocomo.co.jp/service/imode/make/content/pictograph/basic/index.html"
+my @url = (
+    URI->new("http://www.nttdocomo.co.jp/service/imode/make/content/pictograph/basic/index.html"),
+    URI->new("http://www.nttdocomo.co.jp/english/service/imode/make/content/pictograph/basic/index.html"),
+    URI->new("http://www.nttdocomo.co.jp/service/imode/make/content/pictograph/extention/index.html"),
+    URI->new("http://www.nttdocomo.co.jp/english/service/imode/make/content/pictograph/extention/index.html"),
 );
-my $scraper = scraper {
-    process 'tr', 'hoge[]', scraper {
-        process 'td:nth-child(5)', 'unicode', 'TEXT';
-        process 'td:nth-child(6)', 'jpname',  'TEXT';
+
+my $res;
+my $i;
+my @prev;
+for my $uri (@url) {
+    my $scraper = scraper {
+        process 'tr', 'characters[]', scraper {
+            process 'td:nth-child(3)', 'sjis', 'TEXT';
+            process 'td:nth-child(5)', 'unicode', 'TEXT';
+            process 'td:nth-child(6)', 'name',  'TEXT';
+        };
     };
-};
-my $result = $scraper->scrape($uri);
+    my @chars = @{ $scraper->scrape($uri)->{characters} };
+
+    # remove headers
+    shift @chars; shift @chars;
+
+    if (++$i % 2) {
+        @prev = @chars;
+    } else {
+        @prev == @chars or die "ja/en count doesn't match";
+        for my $c (0..$#prev) {
+            $prev[$c]->{en_name} = $chars[$c]->{name};
+        }
+        push @$res, @prev;
+    }
+}
+
 binmode STDOUT, ":utf8";
-print Dump($result);
+print Dump($res);
+

093f734757f1fade10315ba8ff679cfe24668728
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2007-11-13 01:28:11 +0000

    Encode-JP-Mobile: added docomo unicode 2 japanese name scraping script.

---
+++ tools/docomo-uni2name.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Web::Scraper;
+use URI;
+use YAML;
+
+my $uri = URI->new(
+    "http://www.nttdocomo.co.jp/service/imode/make/content/pictograph/basic/index.html"
+);
+my $scraper = scraper {
+    process 'tr', 'hoge[]', scraper {
+        process 'td:nth-child(5)', 'unicode', 'TEXT';
+        process 'td:nth-child(6)', 'jpname',  'TEXT';
+    };
+};
+my $result = $scraper->scrape($uri);
+print Dump($result);

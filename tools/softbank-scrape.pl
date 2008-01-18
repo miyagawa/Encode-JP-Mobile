7dbf245c5264e62a8e2af99ec1ea8a3ad9b4ba1c
miyagawa <miyagawa@d0d07461-0603-4401-acd4-de1884942a52>
2008-01-18 04:57:50 +0000

    add image and number to Softbank conversion table

--- tools/softbank-scrape.pl
+++ tools/softbank-scrape.pl
@@ -5,12 +5,18 @@ use Web::Scraper;
 use URI;
 use YAML;
 
+my $number = 1;
+
 my $emoji = scraper {
   process '//table[@width="100%" and @cellpadding="2"]//tr/td/font/../..',
-    'emoji[]' => scraper {
-      process '//td[2]/font', unicode => 'TEXT';
-      process '//td[3]/font', sjis => [ 'TEXT', sub { unpack "H*", shift } ];
-    };
+      'emoji[]' => scraper {
+          # 264-266 are Skymail and 277-280 are J-PHONE chars, removed from their website
+          $number += 4 if $number == 267;
+          process '//td[2]/font', unicode => 'TEXT';
+          process '//td[3]/font', sjis => [ 'TEXT', sub { unpack "H*", shift } ];
+          process '//td[1]/img',  image => [ '@src', sub { $_->as_string } ];
+          process '//td[1]', number => [ 'TEXT', sub { $number++ } ]; # /td[1] etc. is dummy
+      };
   result 'emoji';
 };
 
